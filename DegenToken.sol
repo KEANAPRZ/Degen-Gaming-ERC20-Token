db978f8aa42b128384de339020d4816c560f5ed5
KEANA ALIZA C. PEREZ <137427255+KEANAPRZ@users.noreply.github.com>
2024-06-04 01:19:06 +0800

    Update DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -10,35 +10,40 @@ contract DegenToken is ERC20, Ownable {
   // The redemption cost for magic is set to 100 tokens per unit of magic.
   uint256 public constant REDEMPTION_COST = 100;
 
-  // This mapping keeps track of the address that redeemed the magic (key) and the amount of magic owned (value).
-  mapping(address => uint256) public magicOwnedBy; // Changed name for clarity
+  // Enumeration for different types of magic
+  enum MagicType { FIRE, ICE, WATER, EARTH, WIND }// 1,2,3,4,5 id of ff
+
+  // This mapping keeps track of the address that redeemed the magic (key) and the amount of each type of magic owned (value).
+  mapping(address => mapping(MagicType => uint256)) public magicOwnedBy;
 
   // The constructor initializes the contract with an initial supply of tokens for the contract creator.
   constructor() 
-    ERC20("Degen", "DGN") Ownable(msg.sender) {
+    ERC20("Degen", "DGN")
+    Ownable(msg.sender) // Pass msg.sender as the initial owner to the Ownable constructor
+  {
     _mint(msg.sender, 10 * (10 ** uint256(decimals())));
   }
 
-  // This function allows users to redeem their DegenTokens for a "powerful magic" at a cost of 100 tokens per unit of magic.
+  // This function allows users to redeem their DegenTokens for a specified type of magic at a cost of 100 tokens per unit of magic.
   // The function burns the tokens and updates the user's magic balance in the `magicOwnedBy` mapping.
-  function redeemMagic(uint256 quantity) public {
+  function redeemMagic(MagicType magicType, uint256 quantity) public {
     uint256 cost = REDEMPTION_COST * quantity;
-    require(balanceOf(msg.sender) >= cost, "Your token is not enough tokens to redeem for a powerful magic");
+    require(balanceOf(msg.sender) >= cost, "Your token balance is not enough to redeem for a powerful magic");
 
-    magicOwnedBy[msg.sender] += quantity;
+    magicOwnedBy[msg.sender][magicType] += quantity;
     _burn(msg.sender, cost);
   }
 
-  // This function returns the amount of magic owned by a specified address (considering the mapped redeemer).
-  function checkMagicOwned(address user) public view returns (uint256) {
-    return magicOwnedBy[user];
+  // This function returns the amount of a specific type of magic owned by a specified address.
+  function checkMagicOwned(address user, MagicType magicType) public view returns (uint256) {
+    return magicOwnedBy[user][magicType];
   }
 
-  // This function allows the contract owner to assign the redeemed magic (stored in magicOwned) to a designated recipient address.
-  function grantMagic(address recipient, uint256 amount) public onlyOwner {
-    require(magicOwnedBy[msg.sender] >= amount, "Insufficient redeemed magic to grant");
-    magicOwnedBy[msg.sender] -= amount;
-    magicOwnedBy[recipient] += amount;
+  // This function allows the contract owner to assign the redeemed magic (stored in magicOwnedBy) to a designated recipient address.
+  function grantMagic(address recipient, MagicType magicType, uint256 amount) public onlyOwner {
+    require(magicOwnedBy[msg.sender][magicType] >= amount, "Insufficient redeemed magic to grant");
+    magicOwnedBy[msg.sender][magicType] -= amount;
+    magicOwnedBy[recipient][magicType] += amount;
   }
 
   // This function allows the contract owner to mint new tokens and send them to a specified address (for potential magic redemption).
@@ -58,14 +63,14 @@ contract DegenToken is ERC20, Ownable {
 
   // This function allows users to burn their tokens, removing them from circulation.
   function burnTokens(uint256 amount) public {
-    require(balanceOf(msg.sender) >= amount, "Your token is not enough tokens to burn");
+    require(balanceOf(msg.sender) >= amount, "Your token balance is not enough to burn");
     _burn(msg.sender, amount);
   }
 
   // This function allows users to transfer their tokens to another address.
   function transferTokens(address to, uint256 amount) public {
-    require(to != address(0), "Your address is invalid ");
-    require(balanceOf(msg.sender) >= amount, "Your token is not enough tokens to transfer");
+    require(to != address(0), "The address is invalid");
+    require(balanceOf(msg.sender) >= amount, "Your token balance is not enough to transfer");
     _transfer(msg.sender, to, amount);
   }
 }
